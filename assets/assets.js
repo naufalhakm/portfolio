bd13aed677067752334629e2b5ef2c757b9ee842
Naufal Hakim <naufalhakim6b@gmail.com>
2025-04-15 09:45:03 +0700

    first commit

---
+++ assets/assets.js
@@ -0,0 +1,124 @@
+import user_image from './user-image.png';
+import code_icon from './code_icon.png';
+import code_icon_dark from './code-icon-dark.png';
+import edu_icon from './edu_icon.png';
+import edu_icon_dark from './edu-icon-dark.png';
+import project_icon from './project_icon.png';
+import project_icon_dark from './project-icon-dark.png';
+import docker from './docker.png';
+import firebase from './firebase.png';
+import aws from './aws.png';
+import gcp from './gcp.png';
+import rabbit from './rabbit.png';
+import git from './git.png';
+import mongodb from './mongodb.png';
+import right_arrow_white from './right-arrow-white.png';
+import logo from './logo.png';
+import logo_dark from './logo_dark.png';
+import mail_icon from './mail_icon.png';
+import mail_icon_dark from './mail_icon_dark.png';
+import profile_img from './profile-img.png';
+import download_icon from './download-icon.png';
+import hand_icon from './hand-icon.png';
+import header_bg_color from './header-bg-color.png';
+import moon_icon from './moon_icon.png';
+import sun_icon from './sun_icon.png';
+import arrow_icon from './arrow-icon.png';
+import arrow_icon_dark from './arrow-icon-dark.png';
+import menu_black from './menu-black.png';
+import menu_white from './menu-white.png';
+import close_black from './close-black.png';
+import close_white from './close-white.png';
+import web_icon from './web-icon.png';
+import mobile_icon from './mobile-icon.png';
+import ui_icon from './ui-icon.png';
+import graphics_icon from './graphics-icon.png';
+import right_arrow from './right-arrow.png';
+import send_icon from './send-icon.png';
+import right_arrow_bold from './right-arrow-bold.png';
+import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+import poto from './poto.png';
+import formal from './formal.png';
+import santai from './santai.png';
+
+export const assets = {
+    user_image,
+    code_icon,
+    code_icon_dark,
+    edu_icon,
+    edu_icon_dark,
+    project_icon,
+    project_icon_dark,
+    aws,
+    firebase,
+    gcp,
+    docker,
+    git,
+    rabbit,
+    mongodb,
+    right_arrow_white,
+    logo,
+    logo_dark,
+    mail_icon,
+    mail_icon_dark,
+    profile_img,
+    download_icon,
+    hand_icon,
+    header_bg_color,
+    moon_icon,
+    sun_icon,
+    arrow_icon,
+    arrow_icon_dark,
+    menu_black,
+    menu_white,
+    close_black,
+    close_white,
+    web_icon,
+    mobile_icon,
+    ui_icon,
+    graphics_icon,
+    right_arrow,
+    send_icon,
+    right_arrow_bold,
+    right_arrow_bold_dark,
+    poto,
+    formal,
+    santai
+};
+
+export const workData = [
+    {
+        title: 'Arkademi',
+        description: 'Backend Engineer (Laravel/Go)',
+        bgImage: '/work-6.png',
+        link: 'https://www.arkademi.com'
+    },
+    {
+        title: 'Arkademi Prakerja',
+        description: 'Backend Engineer (Laravel)',
+        bgImage: '/work-8.png',
+        link: 'https://www.arkademi.com'
+    },
+    {
+        title: 'Intelitalk',
+        description: 'AI Apps HR (Go/Python)',
+        bgImage: '/work-7.png',
+        link: '#'
+    },
+    {
+        title: 'SweatSpark',
+        description: 'Dating apps (Go)',
+        bgImage: '/work-4.png',
+        link: '#'
+    },
+]
+
+export const infoList = [
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'Go, Laravel, JavaScript, Python' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'Computer Science - Binus\nComputer Engineer - IPB' },
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
+];
+
+export const toolsData = [
+    assets.docker, assets.firebase, assets.mongodb, assets.aws, assets.git, assets.gcp, assets.rabbit
+];
