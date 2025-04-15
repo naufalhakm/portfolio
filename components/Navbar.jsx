4d4e5ac909f2393f6b4a035bd9a1b87067707d49
Naufal Hakim <naufalhakim6b@gmail.com>
2025-04-15 11:04:30 +0700

    fix: update navbar blur

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
             <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
                 <Image src={assets.header_bg_color} alt='' className='w-full' />
             </div>
-            <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""}`}>
+            <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white/30 backdrop-blur-lg shadow-sm" : ""}`}>
                 <a href="#top">
                     <Image src={assets.logo} alt="" className='w-35
                 cursor-pointer mr-14'/>
